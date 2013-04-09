d6ca8e10e9d260628b87d6654f7d4ff887aa05ba
vquanghuy@gmail.com <vquanghuy@gmail.com@f4256e53-5f71-3d3e-adab-91e49e9e7c3b>
2013-04-09 08:17:43 +0000

    - add set SDK function

--- trunk/ConfigForm.cs
+++ trunk/ConfigForm.cs
@@ -11,9 +11,37 @@ namespace BLog
 {
     public partial class ConfigForm : Form
     {
+        public String strADBPath = null;
+
         public ConfigForm()
         {
             InitializeComponent();
+        }
+
+        private void btnChooseSDK_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrwDlg = new FolderBrowserDialog();
+            folderBrwDlg.SelectedPath = "c:\\";
+            folderBrwDlg.ShowNewFolderButton = false;
+
+            if (folderBrwDlg.ShowDialog() == DialogResult.OK)
+            {
+                txbSDKPath.Text = folderBrwDlg.SelectedPath;
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            strADBPath = txbSDKPath.Text + "\\platform-tools\\adb.exe";
+            if (System.IO.File.Exists(strADBPath))
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {   
+                MessageBox.Show(this, "SDK Path not correct");
+            }            
         }
     }
 }
