8e506120d67c6648bb40ede3bb1d0a1e7b06a65f
vquanghuy@gmail.com <vquanghuy@gmail.com@f4256e53-5f71-3d3e-adab-91e49e9e7c3b>
2013-04-09 07:50:06 +0000

    + add Config Form

---
+++ trunk/ConfigForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace BLog
+{
+    partial class ConfigForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabConfigPath = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabConfigPath.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabConfigPath
+            // 
+            this.tabConfigPath.Controls.Add(this.tabPage1);
+            this.tabConfigPath.Controls.Add(this.tabPage2);
+            this.tabConfigPath.Location = new System.Drawing.Point(12, 12);
+            this.tabConfigPath.Name = "tabConfigPath";
+            this.tabConfigPath.SelectedIndex = 0;
+            this.tabConfigPath.Size = new System.Drawing.Size(668, 349);
+            this.tabConfigPath.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(660, 323);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Config Path";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(660, 323);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ConfigForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 373);
+            this.Controls.Add(this.tabConfigPath);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConfigForm";
+            this.Text = "Config ...";
+            this.tabConfigPath.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabConfigPath;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
